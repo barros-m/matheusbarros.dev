934c3b2921cb78be1da5d69a434039d9f5340fd4
Matheus Barros <matheusbarros1305@gmail.com>
2024-06-30 17:52:09 -0400

    Improved navbar

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section id="hero" className="bg-white h-screen flex flex-col justify-center items-center">
       <h1 className="text-5xl font-bold mb-4">Hi, I'm Matheus Barros</h1>
-      <p className="text-xl mb-8">A Full Stack Developer & Data Analyst</p>
+      <p className="text-xl mb-8">Crafting Innovative Solutions with Code and Data</p>
       <a href="#connect" className="px-4 py-2 bg-blue-500 text-white rounded">Let's Connect</a>
     </section>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,43 @@
 import React from 'react';
 
 const NavBar = () => {
+
+    const options = [
+        {
+            label: 'About',
+            href: '#about',
+        },
+        {
+            label: 'Skills',
+            href: '#tech-stacks',
+        },
+        {
+            label: 'Projects',
+            href: '#projects',
+        },
+        {
+            label: 'Let\'s Connect',
+            href: '#connect',
+        }
+    ];
+
   return (
-    <nav className="bg-white shadow-md">
-      <div className="container mx-auto px-4 py-2 flex justify-between items-center">
+    <nav className="bg-white shadow-md pt-3">
+      <div className="container mx-auto px-4 py-2 flex justify-between items-center max-w-screen-lg">
         <div className="text-2xl font-bold">Matheus Barros</div>
-        <div>
-          <a href="#hero" className="px-4">Home</a>
-          <a href="#about" className="px-4">About</a>
-          <a href="#tech-stacks" className="px-4">Skills</a>
-          <a href="#projects" className="px-4">Projects</a>
-          <a href="#connect" className="px-4">Let's Connect</a>
+        <div className="flex space-x-4">
+            {
+                options.map((option) => {
+                    return(
+                        <a href={option.href} className="px-4 pb-1 relative group">
+                            {option.label}
+                            <span className="block absolute bottom-0 left-0 w-full h-0.5 bg-green-900 transform scale-x-0 group-hover:scale-x-100 transition-transform origin-left duration-300"></span>
+                        </a>
+                    )
+                })
+            }
+          
+          
         </div>
       </div>
     </nav>
